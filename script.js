9deb0d41fa2e2dd459c8dc6159dbb33f33f94f2b
ChocoMilk7 <t.chocomilk.s@gmail.com>
2025-05-11 11:30:53 +0700

    Update script.js

--- script.js
+++ script.js
@@ -33,3 +33,9 @@ gsap.from('.title', {opacity: 0, duration: 1, delay: 1.6, y:30})
 gsap.from('.description', {opacity: 0, duration: 1, delay: 1.8, y:30})
 gsap.from('.btn', {opacity: 0, duration: 1, delay: 2.1, y:30})
 gsap.from('.image', {opacity: 0, duration: 1, delay: 2.6, y:30})
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/service-worker.js')
+    .then(reg => console.log('Service worker registered'))
+    .catch(err => console.error('Service worker registration failed:', err));
+}
